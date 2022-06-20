f995d87aeb520ca141cac949706c586f05b3d8a5
Simon Tinsley <45486136+sjtinsley@users.noreply.github.com>
2022-06-20 16:28:14 +0200

    Project skeleton setup and basic balance changing functionality implemented

---
+++ spec/account_spec.rb
@@ -0,0 +1,23 @@
+require 'account'
+
+describe Account do
+
+  it "initializes with a balance of zero" do
+    expect(subject.balance).to eq 0
+  end
+
+  describe "#deposit" do
+    it "add an amount to the balance" do
+      subject.deposit(500)
+      expect(subject.balance).to eq 500
+    end
+  end
+
+  describe "#withdraw" do
+    it "removes an amount from the balance" do
+      subject.deposit(500)
+      subject.withdraw(200)
+      expect(subject.balance).to eq 300
+    end
+  end
+end
